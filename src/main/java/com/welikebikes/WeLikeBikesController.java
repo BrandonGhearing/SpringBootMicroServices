f591435eadcc476004465268181e4f9511151e0e
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-26 23:33:57 -0400

    add javadoc, add default / endpoint

---
+++ src/main/java/com/welikebikes/WeLikeBikesController.java
@@ -0,0 +1,25 @@
+package com.welikebikes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WeLikeBikesController 
+{
+	/**
+	 * Handle the /start endpoint
+	 * @return
+	 */
+	
+	@RequestMapping("/start")
+	public String start() 
+	{
+		return "start";
+	}
+	
+	@RequestMapping("/")
+	public String index() 
+	{
+		return "start";
+	}
+}
