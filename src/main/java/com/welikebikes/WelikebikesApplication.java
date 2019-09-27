429fe378f52a5ea4cb6ed4ff806698e4afc1fd46
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-26 23:06:30 -0400

    Initial commit of Spring boot Project

---
+++ src/main/java/com/welikebikes/WelikebikesApplication.java
@@ -0,0 +1,13 @@
+package com.welikebikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelikebikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelikebikesApplication.class, args);
+	}
+
+}
